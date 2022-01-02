461d2a7eff3aaf97ce0ba593a8034ba5674118a7
LABOIRIE Alex <Alex.Laboirie@bordeaux-inp.fr>
2022-01-02 18:59:20 +0100

    changed header

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(session({
     resave: false
 }));
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', 'https://alloeirb.herokuapp.com');
     res.setHeader("Access-Control-Allow-Credentials" ,'true');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
