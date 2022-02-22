6187de8f727e468cae7f2fc8aeb3310f05cbf0b4
LABOIRIE Alex <Alex.Laboirie@bordeaux-inp.fr>
2022-02-22 22:55:09 +0100

    better account creatio

--- controller/userController.js
+++ controller/userController.js
@@ -34,6 +34,7 @@ module.exports.atemptAuthentification = async (req, res) => {
             let orderid = id;
             let  codeObject = {}
             let allCodes = getCodes()
+            console.log(codes)
             console.log(typeof codes)
             if(codes !== null){
                 codes.split(",").map(c => {
