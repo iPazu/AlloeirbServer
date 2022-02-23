36004e67a9c75cc48fe1f17b693f2a43e2542b74
LABOIRIE Alex <Alex.Laboirie@bordeaux-inp.fr>
2022-02-23 18:39:19 +0100

    switching to prod

--- sql/orderRequest.js
+++ sql/orderRequest.js
@@ -116,6 +116,8 @@ async function updateStock(productdata){
             p = p.replace("{","")
             p = p.replace("}","")
             p = p.replaceAll('"',"")
+            p = p.replaceAll("'","")
+            p = p.replaceAll(" ","")
             p = p.split(":")
             let id = p[0]
             let quantity = p[1]
